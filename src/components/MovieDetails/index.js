7517885742b7347202394a760d58f518b85fab1a
austinmpask <austinmpask@gmail.com>
2024-02-29 16:11:12 -0800

    done

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,15 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+  const movieChoice = movies.find((movie) => {
+    return movie.id === parseInt(movieId);
+  });
+  console.log(movieChoice);
   return (
-    <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+    <div className="comp purple">
+      <h1>{movieChoice.title}</h1>
+      <p>{movieChoice.description}</p>
     </div>
   );
 }
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,20 @@
-function Movies() {
+import { NavLink, Route } from "react-router-dom/cjs/react-router-dom.min";
+import MovieDetails from "../MovieDetails/index";
+
+function Movies({ movies }) {
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movie) => (
+          <NavLink key={movie.id} to={`/movies/${movie.id}`}>
+            {movie.title}
+          </NavLink>
+        ))}
+      </nav>
+      <Route path="/movies/:movieId">
+        <MovieDetails movies={movies} />
+      </Route>
     </div>
   );
 }
